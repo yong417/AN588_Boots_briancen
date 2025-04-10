b3ce640ac1638fd39c1b6380a6dba542383cbc80
yongcen417 <yongcen22100@gmail.com>
2025-04-10 09:36:46 -0400

    Push

---
+++ README.m
@@ -0,0 +1,16 @@
+## Challenges Faced During the Assignment
+
+1. **Data Subsetting**  
+   At the beginning, I did not inspect the data carefully enough, so I did not notice that there were NA values in the dataset. I ran into some errors and had to look up how to correctly filter the data.
+
+2. **Using For Loops**  
+   I found using for loops for bootstrapping was not very familiar to me even though we have done it before. I needed to write out several steps of the loop on paper before I could understand the process, and setting up the correct indices was particularly challenging.
+
+3. **Updating the Matrix**  
+   Before starting the for loop, I had to create an empty matrix to store all the information generated during each iteration. Deciding on the structure of the matrix and which variables to include was not straightforward.
+
+4. **Plotting to Compare Bootstrap and the True lm() Model**  
+   There were many elements to store and plot on the same graphs. I found it difficult to determine the best way to display both the bootstrap estimates and the full model results together.
+
+5. **Adjusting the Legend in the Plot**  
+   Once the plot was complete, I discovered that the legend was blocking part of the graph. I had to modify the legend settings so that it appears outside the main plot area.
